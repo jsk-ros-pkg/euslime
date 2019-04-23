44556c5fd5fb4dac1ed2978bc00a770ae07f3980
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2019-04-23 12:51:02 +0900

    Add more tests

--- euslime_tests.py
+++ euslime_tests.py
@@ -90,6 +90,52 @@ class EuslimeTestBase(unittest.TestCase):
             '(:read-aborted 0 1)',
             '(:return (:ok nil) 33)')
 
+    def test_swank_eval_3(self):
+        self.assertSocket(
+            '(:emacs-rex (swank-repl:listener-eval "list 1 2 3\n") "USER" :repl-thread 5)',
+            '(:read-string 0 1)',
+            '(:write-string "(1 2 3)" :repl-result)',
+            '(:write-string "\\n" :repl-result)',
+            '(:read-aborted 0 1)',
+            '(:return (:ok nil) 5)')
+
+    def test_swank_eval_4(self):
+        self.assertSocket(
+            '(:emacs-rex (swank-repl:listener-eval "(progn (setq *package* *lisp-package*) (send *package* :name))\n") "USER" :repl-thread 30)',
+            '(:read-string 0 1)',
+            '(:write-string "\\"LISP\\"" :repl-result)',
+            '(:write-string "\\n" :repl-result)',
+            '(:read-aborted 0 1)',
+            '(:new-package "LISP" "LISP:irteusgl")',
+            '(:return (:ok nil) 30)')
+        self.assertSocket(
+            '(:emacs-rex (swank-repl:listener-eval "(progn (setq *package* *user-package*) (send *package* :name))\n") "LISP" :repl-thread 31)',
+            '(:read-string 0 1)',
+            '(:write-string "\\"USER\\"" :repl-result)',
+            '(:write-string "\\n" :repl-result)',
+            '(:read-aborted 0 1)',
+            '(:new-package "USER" "irteusgl")',
+            '(:return (:ok nil) 31)')
+
+    def test_swank_eval_5(self):
+        self.assertSocket(
+            '(:emacs-rex (swank-repl:listener-eval "(print \\"this\\")\n") "USER" :repl-thread 43)',
+            '(:read-string 0 1)',
+            '(:write-string "\\"this\\"\\n")',
+            '(:write-string "\\"this\\"" :repl-result)',
+            '(:write-string "\\n" :repl-result)',
+            '(:read-aborted 0 1)',
+            '(:return (:ok nil) 43)')
+
+    def test_swank_eval_6(self):
+        self.assertSocket(
+            '(:emacs-rex (swank-repl:listener-eval "(format nil \\"~S\\" \\"this\\")\n") "USER" :repl-thread 41)',
+            '(:read-string 0 1)',
+            '(:write-string "\\"\\\\\\"this\\\\\\"\\"" :repl-result)',
+            '(:write-string "\\n" :repl-result)',
+            '(:read-aborted 0 1)',
+            '(:return (:ok nil) 41)')
+
     # AUTODOC
     def test_swank_autodoc_1(self):
         self.assertSocket(
@@ -135,6 +181,119 @@ class EuslimeTestBase(unittest.TestCase):
         self.assertSocket(
             '(:emacs-rex (swank:autodoc (quote ("find" "4" (("1" "." "a") ("2" "." "b") ("3" "." "c") ("4" "." "d") ("4" "." "e")) ":key" "" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 29)',
             '(:return (:ok ("(find item seq &key start end test test-not ===> key <=== (count 1))" t)) 29)')
+
+    def test_swank_autodoc_10(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:autodoc (quote ("setq" "" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 6)',
+            '(:return (:ok ("(setq &rest ===> forms <===)" t)) 6)')
+
+    def test_swank_autodoc_11(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:autodoc (quote ("list" "*prompt-string*" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 24)',
+            '(:return (:ok ("*prompt-string* => \\"irteusgl\\"" nil)) 24)')
+
+    def test_swank_autodoc_12(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:autodoc (quote ("print" "" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 30)',
+            '(:return (:ok ("(print ===> obj <=== &optional stream)" t)) 30)')
+
+    def test_swank_autodoc_13(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:autodoc (quote ("print" "\\"this\\"" "" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 31)',
+            '(:return (:ok ("(print obj &optional ===> stream <===)" t)) 31)')
+
+    def test_swank_autodoc_14(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:autodoc (quote ("print" "\\"this\\"" "nil" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 32)',
+            '(:return (:ok ("(print obj &optional ===> stream <===)" t)) 32)')
+
+    def test_swank_autodoc_15(self):
+        self.assertSocket(
+            '(:emacs-rex (swank-repl:listener-eval "(defun foo (a b &rest args &key c &allow-other-keys)\n            (list a b c))\n") "USER" :repl-thread 20)',
+            '(:read-string 0 1)',
+            '(:write-string "foo" :repl-result)',
+            '(:write-string "\\n" :repl-result)',
+            '(:read-aborted 0 1)',
+            '(:return (:ok nil) 20)')
+        self.assertSocket(
+            '(:emacs-rex (swank:autodoc (quote ("foo" "" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 21)',
+            '(:return (:ok ("(foo ===> a <=== b &rest args &key c &allow-other-keys)" t)) 21)')
+        self.assertSocket(
+            '(:emacs-rex (swank:autodoc (quote ("foo" "1" "" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 22)',
+            '(:return (:ok ("(foo a ===> b <=== &rest args &key c &allow-other-keys)" t)) 22)')
+        self.assertSocket(
+            '(:emacs-rex (swank:autodoc (quote ("foo" "1" "2" "" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 23)',
+            '(:return (:ok ("(foo a b &rest args &key c &allow-other-keys)" t)) 23)')
+        self.assertSocket(
+            '(:emacs-rex (swank:autodoc (quote ("foo" "1" "2" ":c" "" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 24)',
+            '(:return (:ok ("(foo a b &rest args &key ===> c <=== &allow-other-keys)" t)) 24)')
+        self.assertSocket(
+            '(:emacs-rex (swank:autodoc (quote ("foo" "1" "2" ":c" "3" swank::%cursor-marker%)) :print-right-margin 100) "USER" :repl-thread 25)',
+            '(:return (:ok ("(foo a b &rest args &key c &allow-other-keys)" t)) 25)')
+        self.assertSocket(
+            '(:emacs-rex (swank-repl:listener-eval "(foo 1 2 :c 3 :d 4)\n") "USER" :repl-thread 29)',
+            '(:read-string 0 1)',
+            '(:write-string "(1 2 3)" :repl-result)',
+            '(:write-string "\\n" :repl-result)',
+            '(:read-aborted 0 1)',
+            '(:return (:ok nil) 29)')
+
+    # COMPLETIONS
+    def test_swank_completions_1(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:completions "find-if" (quote "USER")) "USER" :repl-thread 11)',
+            '(:return (:ok (("find-if" "find-if-not") "find-if")) 11)')
+
+    def test_swank_completions_2(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:completions "find-if-no" (quote "USER")) "USER" :repl-thread 13)',
+            '(:return (:ok (("find-if-not") "find-if-not")) 13)')
+
+    def test_swank_completions_3(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:completions "*pro" (quote "USER")) "USER" :repl-thread 18)',
+            '(:return (:ok (("*program-name*" "*projected-edges*" "*prompt*" "*prompt-string*") "*pro")) 18)')
+
+    def test_swank_completions_4(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:completions "*prompt" (quote "USER")) "USER" :repl-thread 20)',
+            '(:return (:ok (("*prompt*" "*prompt-string*") "*prompt")) 20)')
+
+    def test_swank_completions_5(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:completions "*prompt-" (quote "USER")) "USER" :repl-thread 23)',
+            '(:return (:ok (("*prompt-string*") "*prompt-string*")) 23)')
+
+    # EMACS INTERRUPT
+    def test_emacs_interrupt(self):
+        self.assertSocket(
+            '(:emacs-rex (swank-repl:listener-eval "(loop)\n") "USER" :repl-thread 5)',
+            '(:read-string 0 1)')
+        self.assertSocket('(:emacs-interrupt 0)',
+                          '(:read-aborted 0 1)',
+                          '(:return (:abort "\'Keyboard Interrupt\'") 5)')
+        self.assertSocket(
+            '(:emacs-rex (swank-repl:listener-eval "10\n") "USER" :repl-thread 6)',
+            '(:read-string 0 1)',
+            '(:write-string "10" :repl-result)',
+            '(:write-string "\\n" :repl-result)',
+            '(:read-aborted 0 1)',
+            '(:return (:ok nil) 6)')
+
+    # SET PACKAGE
+    def test_swank_set_package(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:set-package "LISP") "USER" :repl-thread 33)',
+            '(:return (:ok ("LISP" "LISP:irteusgl")) 33)')
+        self.assertSocket(
+            '(:emacs-rex (swank:set-package "USER") "LISP" :repl-thread 35)',
+            '(:return (:ok ("USER" "irteusgl")) 35)')
+
+    # APROPOS
+    def test_apropos_1(self):
+        self.assertSocket(
+            '(:emacs-rex (swank:apropos-list-for-emacs "prompt" t nil (quote nil)) "USER" :repl-thread 17)',
+            '(:return (:ok ((:designator "*PROMPT*" :variable :not-documented) (:designator "*PROMPT-STRING*" :variable :not-documented) (:designator "LISP::PROMPT" :function "(strm)") (:designator "SLIME::LAST-PROMPT" :variable :not-documented) (:designator "SLIME::SLIME-PROMPT" :function "nil") (:designator "TOPLEVEL-PROMPT" :function "(strm)"))) 17)')
 
 
 if __name__ == '__main__':
