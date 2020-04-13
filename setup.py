383aac5177f2c29afdef6813c8bc0914ca5ecc21
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2020-04-13 18:51:27 +0900

    Don't use setuptools for catkin setup

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py
 
-from setuptools import find_packages
-from setuptools import setup
+from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=find_packages('src'),
+    packages=['euslime'],
     package_dir={'': 'src'},
-    install_requires=open('requirements.txt').readlines(),
-    entry_points={
-        'console_scripts': [
-            'euslime = euslime.cli:main',
-            ],
-        },
 )
 
 setup(**setup_args)
